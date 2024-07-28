b95eeef1c04f797db36da79ae8be99b8e6249e39
Fattah_Sabri <fassabri1@gmail.com>
2024-07-28 23:17:42 +0530

    fourth

--- src/Config/db.js
+++ src/Config/db.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 const User = require('../Models/UserModels')
+require('dotenv').config();
+
+const mongoDBUrl = process.env.MONGODB_URL;
 
 
 
-const mongoDBUrl = "mongodb+srv://fassabri1:QTNgZwGSKiDpudSx@cluster0.gzkbxn6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
 
 const connectDB = async () => {
     try {
